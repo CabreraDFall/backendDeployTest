7715929697d9894bceb7cc0109a5199b5e0b84e8
CabreraDfall <cabreradfall@gmail.com>
2025-06-26 08:26:53 +0200

    Endpoints

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const products = require('./api/products');
+const users = require('./api/users');
+const general = require('./api/general');
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -8,6 +11,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/products', products);
+app.get('/users', users);
+app.get('/general', general);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
